a00cd586108808cedd911d0f2b0ed9df9ca7017a
Marcos Wicket <random.wicket@gmail.com>
2016-05-04 17:45:59 -0300

    arrow keys

--- star/js/star.js
+++ star/js/star.js
@@ -1,43 +1,80 @@
 function Star (size, alphaSpeed) {
 	if (size === undefined) { size = 40; }
 	//if (color === undefined) { color = "#ffffff"; }
+	
+	//position variables
 	this.x = 0;
 	this.y = 0;
 	this.centerX = 0;
 	this.centerY = 0;
 	this.size = size;
+
+	//movement variables
 	this.vx = 0;
 	this.vy = 0;
+	this.a = 1;
+	this.moveIteration = Math.floor(Math.random() * 721);
+	this.fallX = this.x;
+	this.fallY = this.y;
+	this.gravity = 2;
+	this.keyPressed = 0;
+
+	//physics variables
 	this.mass = 1;
 	this.rotation = 0;
 	this.scaleX = 1;
 	this.scaleY = 1;
 	this.angle = 0.1;
+
+	//visual variables
 	this.alpha = 1;
-	this.alphaMode = 1;
-	this.moveIteration = Math.floor(Math.random() * 721);
-	this.moveMode = Math.floor(Math.random() * 2);
 	this.r = Math.floor(Math.random()*256);
 	this.g = this.r;
 	this.b = (this.r > 125) ? 256 : Math.random()*256;
 	this.color = 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.alpha + ')';
 	this.lineWidth = 1;
 	this.alphaSpeed = alphaSpeed;
+
+	//misc flags
+	this.alphaMode = 1;
+	this.moveMode = Math.floor(Math.random() * 2);
+	this.destroyWorldFlag = false;
+	this.activateFall = false;
+
 }
 
 Star.prototype.updateMove = function () {
-	if(this.moveMode) {
-		this.moveIteration++;
-		this.angle = 0.011 * this.moveIteration;
-		this.x = this.centerX + (1 + this.angle)*Math.cos(this.angle);
-		this.y = this.centerY + (1 + this.angle)*Math.sin(this.angle);
-		if(this.moveIteration > 719) { this.moveMode = 0; }
-	} else {
-		this.moveIteration--;
-		this.angle = 0.011 * this.moveIteration;
-		this.x = this.centerX + (1 + this.angle)*Math.cos(this.angle);
-		this.y = this.centerY + (1 + this.angle)*Math.sin(this.angle);
-		if(this.moveIteration < 1) { this.moveMode = 1; }
+	if(this.keyPressed == 0 && !this.activateFall) {
+		if(this.moveMode) {
+			this.moveIteration++;
+			this.angle = 0.011 * this.moveIteration;
+			this.x = this.centerX + (1 + this.angle)*Math.cos(this.angle);
+			this.y = this.centerY + (1 + this.angle)*Math.sin(this.angle);
+			if(this.moveIteration > 719) { this.moveMode = 0; }
+		} else {
+			this.moveIteration--;
+			this.angle = 0.011 * this.moveIteration;
+			this.x = this.centerX + (1 + this.angle)*Math.cos(this.angle);
+			this.y = this.centerY + (1 + this.angle)*Math.sin(this.angle);
+			if(this.moveIteration < 1) { this.moveMode = 1; }
+		}
+	}
+	if(this.keyPressed == 1) {	//up
+		this.vy -= this.a;
+		this.y += this.vy;
+		if(this.y < 0) { this.keyPressed = -1; }
+	} else if(this.keyPressed == 2) {	//down
+		this.vy += this.a;
+		this.y += this.vy;
+		if(this.y > window.innerHeight) { this.keyPressed = -1; }
+	} else if(this.keyPressed == 3) {	//left
+		this.vx -= this.a;
+		this.x += this.vx;
+		if(this.x < 0) { this.keyPressed = -1; }
+	} else if(this.keyPressed == 4) {	//right
+		this.vx += this.a;
+		this.x += this.vx
+		if(this.x > window.innerWidth) { this.keyPressed = -1; }
 	}
 };
 
@@ -45,6 +82,7 @@ Star.prototype.draw = function (context) {
 	//context.save();
 	//context.translate(this.x, this.y);
 	/* UPDATE ALPHA FOR THE STARS */
+	
 	if(this.alphaMode) {
 		this.alpha -= this.alphaSpeed;
 		if(this.alpha < 0) { 
@@ -60,6 +98,33 @@ Star.prototype.draw = function (context) {
 	}
 
 	this.updateMove();
+
+	/*if(!this.destroyWorldFlag)	{
+		this.updateMove();
+	} else if (this.activateFall) {
+		this.vy = 5;
+	}*/
+	
+	//this.y += this.vy;
+	if(this.y > canvas.height) { 
+		this.keyPressed = -1;
+		this.y = window.innerHeight - this.size; 
+		this.vy = 0; 
+	} else if(this.y < 0) {
+		this.keyPressed = -1;
+		this.y = this.size;
+		this.vy = 0;
+	}
+
+	if(this.x > canvas.width) {
+		this.keyPressed = -1;
+		this.x = window.innerWidth - this.size;
+		this.vx = 0;
+	} else if(this.x < 0) {
+		this.keyPressed = -1;
+		this.x = this.size;
+		this.vx = 0;
+	}
 
 	/* UPDATE ALPHA IN FILLSTYLE */
 	this.color = 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.alpha + ')';
@@ -77,3 +142,13 @@ Star.prototype.getBounds = function() {
 		height: this.radius * 2
 	};
 };
+
+Star.prototype.destroyWorld = function (destroyWorldSignal) {
+	this.destroyWorldFlag = destroyWorldSignal;
+	this.activateFall = true;
+};
+
+Star.prototype.gravityChange = function (keyPressed) {
+	this.keyPressed = keyPressed;
+	this.activateFall = true;
+}
--- star/js/test.js
+++ star/js/test.js
@@ -2,7 +2,37 @@ window.onload = function () {
 	var canvas = document.getElementById('canvas'),
 	context = canvas.getContext('2d'),
 	particles = [],
-	numSquares = window.innerWidth * 0.9;
+	numSquares = window.innerWidth * 0.9,
+	destroyWorldSignal = false,
+	keyPressed = 0;
+
+	document.onkeydown = checkKey;
+
+	function checkKey(e) {
+	    e = e || window.event;
+
+	    //up
+	    if (e.keyCode == '38') {
+	        keyPressed = 1;
+	    }
+	    //down
+	    else if (e.keyCode == '40') {
+	        keyPressed = 2;
+	    }
+	    //left
+	    else if (e.keyCode == '37') {
+	       keyPressed = 3;
+	    }
+	    //right
+	    else if (e.keyCode == '39') {
+	       keyPressed = 4;
+	    }
+	    for(var i, i = 0 ; i < numSquares ; i++) {
+			particles[i].gravityChange(keyPressed);
+		}
+	}
+
+	document.addEventListener("click", destroyWorld, false);
 
 	generateBlocks(numSquares);
 
@@ -18,6 +48,17 @@ window.onload = function () {
 			particle.alpha = 1;
 
 			particles.push(particle);
+		}
+	}
+
+	function destroyWorld() {
+		if(destroyWorldSignal) {
+			destroyWorldSignal = false;
+		} else {
+			destroyWorldSignal = true;
+		}
+		for(var i, i = 0 ; i < numSquares ; i++) {
+			particles[i].destroyWorld(destroyWorldSignal);
 		}
 	}
 
