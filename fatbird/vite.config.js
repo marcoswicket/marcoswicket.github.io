b314dcd4960185ed4dfce9c164613459a8238a3c
Marcos Wicket <marcos.wicket@protonmail.com>
2024-12-25 11:41:19 -0300

    vite trying to fix import

--- fatbird/vite.config.js
+++ fatbird/vite.config.js
@@ -4,5 +4,18 @@ export default defineConfig({
 	base: '/fatbird/',
 	build: {
 		outDir: 'dist',
+		rollupOptions: {
+			external: [],
+		},
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		}
+	},
+	resolve: {
+		dedupe: ['pixi.js', 'matter-js'],
+		alias: {
+			'pixi.js': 'pixi.js',
+			'matter-js': 'matter-js'
+		}
 	}
 })
