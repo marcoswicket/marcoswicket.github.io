cf28c9a3a7c61fc6f2518e0617be182539042ad7
Marcos Wicket <marcos.wicket@protonmail.com>
2024-12-25 04:04:02 -0300

    changing to base folder

--- fatbird/vite.config.js
+++ fatbird/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: 'marcoswicket.github.io',
+    base: '/fatbird/',
     assetsInclude: ['**/*.vert', '**/*.frag'],
     build: {
         outDir: 'dist',
