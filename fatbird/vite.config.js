e1632d23850fa59f688f0e977f56f2d3137b84f5
Marcos Wicket <marcos.wicket@protonmail.com>
2024-12-25 04:03:08 -0300

    trying deploy on github

--- fatbird/vite.config.js
+++ fatbird/vite.config.js
@@ -2,19 +2,19 @@ import { defineConfig } from "vite"
 import { resolve } from 'path'
 
 export default defineConfig({
-	base: './',
-	assetsInclude: ['**/*.vert', '**/*.frag'],
-	build: {
-		outDir: 'dist',
-		target: 'esnext',
-		sourcemap: true,
-	},
-	resolve: {
-		alias: {
-			'pixi.js': resolve(__dirname, 'node_modules/pixi.js/dist/pixi.mjs'),
-		}
-	},
-	optimizeDeps: {
-		include: ['matter-js']
-	}
+    base: 'marcoswicket.github.io',
+    assetsInclude: ['**/*.vert', '**/*.frag'],
+    build: {
+        outDir: 'dist',
+        target: 'esnext',
+        sourcemap: true,
+    },
+    resolve: {
+        alias: {
+            'pixi.js': resolve(__dirname, 'node_modules/pixi.js/dist/pixi.mjs'),
+        }
+    },
+    optimizeDeps: {
+        include: ['matter-js']
+    }
 });
